b8b53e01db5f914da27c3e25157ee32b1969b49c
Jonah <jonah.stieglitz17@gmail.com>
2016-05-31 00:23:50 -0500

    Clean Up And Side Note

--- src/main/java/frames/common/views/container/ViewContainer.java
+++ src/main/java/frames/common/views/container/ViewContainer.java
@@ -4,27 +4,23 @@ package frames.common.views.container;
  * Created by Jonah on 5/30/2016.
  */
 
+/*
+    JPanel With The Sole Purpose Of
+    Being Used As The Container For
+    All Of My (Possibly Too Many ;P)
+    CardLayout Views Due To The
+    Issues With Calling "switchView"
+    Methods In A Lot Of Diff. Classes
+    And The Inability To Use this Statically
+ */
+
 import javax.swing.*;
 import java.awt.*;
 
 public class ViewContainer extends JPanel
 {
-
     public ViewContainer(CardLayout cardLayout)
     {
         super(cardLayout);
-
-        initComponents();
-        addComponents();
-    }
-
-    private void initComponents()
-    {
-
-    }
-
-    private void addComponents()
-    {
-
     }
 }
